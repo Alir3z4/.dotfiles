0127d295b78a38b6045248b3f085ad30c3aeff84
Alireza Savand <alireza.savand@gmail.com>
2014-10-20 16:29:16 +0400

    Add dataurl
    Create a data URL from a file

--- .functions.sh
+++ .functions.sh
@@ -254,6 +254,16 @@ function fs() {
 	fi;
 }
 
+
+# Create a data URL from a file
+function dataurl() {
+	local mimeType=$(file -b --mime-type "$1");
+	if [[ $mimeType == text/* ]]; then
+		mimeType="${mimeType};charset=utf-8";
+	fi
+	echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+}
+
 # `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
 # the `.git` directory, listing directories first. The output gets piped into
 # `less` with options to preserve color and line numbers, unless the output is
