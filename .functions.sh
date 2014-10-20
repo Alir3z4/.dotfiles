3b977a5da32cbb6d21f87fd60c52cc4841af1204
Alireza Savand <alireza.savand@gmail.com>
2014-10-20 16:29:31 +0400

    Add gitio
    Create a git.io short URL

--- .functions.sh
+++ .functions.sh
@@ -264,6 +264,15 @@ function dataurl() {
 	echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
 }
 
+# Create a git.io short URL
+function gitio() {
+	if [ -z "${1}" -o -z "${2}" ]; then
+		echo "Usage: \`gitio slug url\`";
+		return 1;
+	fi;
+	curl -i http://git.io/ -F "url=${2}" -F "code=${1}";
+}
+
 # `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
 # the `.git` directory, listing directories first. The output gets piped into
 # `less` with options to preserve color and line numbers, unless the output is
