4a3b50698e01f3025aed71c2060fc92a2ea13bde
Alireza Savand <alireza.savand@gmail.com>
2014-10-20 16:31:44 +0400

    Add unidecode
    Decode \x{ABCD}-style Unicode escape sequences

--- .functions.sh
+++ .functions.sh
@@ -291,6 +291,14 @@ function json() {
 	fi;
 }
 
+# Decode \x{ABCD}-style Unicode escape sequences
+function unidecode() {
+	perl -e "binmode(STDOUT, ':utf8'); print \"$@\"";
+	# print a newline unless we’re piping the output to another program
+	if [ -t 1 ]; then
+		echo ""; # newline
+	fi;
+}
 
 # Get a character’s Unicode code point
 function codepoint() {
