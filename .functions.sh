f478372c6f1beb46adb699e18ebb911870374455
Alireza Savand <alireza.savand@gmail.com>
2014-10-20 16:30:35 +0400

    Add v
    `v` with no arguments opens the current directory in Vim, otherwise
    opens the given location

--- .functions.sh
+++ .functions.sh
@@ -291,6 +291,17 @@ function json() {
 	fi;
 }
 
+
+# `v` with no arguments opens the current directory in Vim, otherwise opens the
+# given location
+function v() {
+	if [ $# -eq 0 ]; then
+		vim .;
+	else
+		vim "$@";
+	fi;
+}
+
 # `o` with no arguments opens the current directory, otherwise opens the given
 # location
 function o() {
